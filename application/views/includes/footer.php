aa4c9ccd2ec60f397e1809c6406ace176c80b64e
Panumas Wiriyachaikit <panumas.wi@outlook.com>
2018-11-22 11:04:51 +0700

    Add files via upload

---
+++ application/views/includes/footer.php
@@ -0,0 +1,21 @@
+  
+  </div> <!-- End Container -->
+
+<!-- Footer -->
+<footer class="page-footer font-small pt-4">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-5">© 2018 Copyright: Max Wiriyachaikit, Email :
+      <a href="mailto:panumas.wi@gmail.com"> panumas.wi@gmail.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+
+    <!-- Loading JavaScript -->
+    <script src="<?php echo base_url();?>assets/jquery/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ application/views/includes/header.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SE.ON.A - SErvice ONline Application</title>
+
+    <!-- Loading Bootstrap -->
+    <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Loading Custom CSS -->
+    <link href="<?php echo base_url();?>assets/custom-css/losa_login.css" rel="stylesheet">
+
+    <!-- Loading Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+</head>
+<body>
+    
+
